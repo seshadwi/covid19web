ae7883d85f6ebac71e95e66d5f22d0090d3ec30b
seshadwi <seshadwi@gmail.com>
2021-03-22 22:55:16 +0700

    modify login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="d-flex justify-content-center align-items-center bg-darkblue position-absolute h-100 w-100">
+    
+    <div class="w-50 m-3">
+        <div class="d-flex flex-column align-items-center">
+            <div class="text-center">
+                <img src="{{ asset('images/app-icon.png') }}" class="icon-app" alt="" srcset="">
+                <h3>Covid-19 Tracker</h3>
             </div>
+            <form class="h-100" action="{{ route('login') }}" method="post">
+                <div class="form-custom-cvd d-flex flex-column py-2">
+                    <label for="username">{{ __('Username') }}</label>
+                    <input type="text" class="@error('username') is-invalid @enderror" name="username" value="{{ old("username") }}" required autocomplete="username" autofocus>
+                </div>
+                <div class="form-custom-cvd d-flex flex-column py-2">
+                    <label for="password">{{ __('Password') }}</label>
+                    <input type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                </div>
+                <div class="form-custom-cvd d-flex flex-column py-2">
+                    <button type="submit" class="btn btn-primary">{{ __('Login') }}</button>
+                    <div class="py-3 d-flex align-items-center flex-nowrap">
+                        <hr class="order-1 hr-flex">
+                        <span class="order-1 mx-2">Registrasi</span>
+                        <hr class="order-1 hr-flex">
+                    </div>
+                    <a class="btn btn-secondary" href="{{ route("register") }}">{{ __('Registrasi') }}</a>
+                </div>
+                
+            </form>
         </div>
     </div>
+    
 </div>
 @endsection
